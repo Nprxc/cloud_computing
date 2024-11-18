cfb2627ad7a5ebabf74c35a033d234a0db842412
Nprxc <guy.axel@orange.fr>
2024-11-18 09:55:55 +0100

    My Signed Commit
    Signed-off-by: Nprxc <guy.axel@orange.fr>

---
+++ infrastructure/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "local" {
+    path = "./.tfstate/terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.3"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.3.0"
+    }
+  }
+  required_version = "~> 1.9"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
